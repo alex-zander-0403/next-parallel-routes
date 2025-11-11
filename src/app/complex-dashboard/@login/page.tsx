f4ec0ebc1822686c79a1b78ee14cc8ce51af44fe
alex-zander-0403 <alexzander0403@gmail.com>
2025-11-11 04:48:22 +0300

    add conditional routes

---
+++ src/app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginSlot() {
+  return (
+    <div className="flex justify-center items-center flex-col gap-5">
+      <h2 className="text-3xl">Авторизация</h2>
+      <div className="flex justify-center items-center w-200 h-100 border rounded-xl border-gray-700">
+        Форма входа
+      </div>
+    </div>
+  );
+}
--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -5,18 +5,28 @@ export default function ComplexDashboardLayout({
   advertising,
   menu,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   advertising: React.ReactNode;
   menu: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
+  const isLoggedIn = true;
+
   return (
     <div>
       <div>{children}</div>
-      <div>{advertising}</div>
-      <div>{menu}</div> 
-      <div>{notifications}</div>
+      {isLoggedIn ? (
+        <>
+          <div>{advertising}</div>
+          <div>{menu}</div>
+          <div>{notifications}</div>
+        </>
+      ) : (
+        login
+      )}
     </div>
   );
 }
